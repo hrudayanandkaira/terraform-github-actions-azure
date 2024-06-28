4bc8e3f38fd09ab21a5c9a3fc67ba3c12d3462e7
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2024-06-28 18:51:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,8 +25,11 @@ provider "azurerm" {
   #client_id       = "c586ee23-af11-497c-8b73-22e8b785dda5"
   #client_secret   = ""
   #client_secret: "${{secrets.client_secret}}"
-  #tenant_id       = "9f2a5891-22b8-4308-92fa-fdef45ffb338"
   #subscription_id = "e520db69-a509-4dbf-9480-fa7a46560ac1"
+   client_id       = "c586ee23-af11-497c-8b73-22e8b785dda5"
+  #client_secret   = var.client_secret
+  tenant_id       = "9f2a5891-22b8-4308-92fa-fdef45ffb338"
+  subscription_id = "e520db69-a509-4dbf-9480-fa7a46560ac1"
   features {}
  # use_oidc = true
 }
