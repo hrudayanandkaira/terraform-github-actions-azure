d1c74c7078e8599ae1553b4292993267a7532f4f
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2024-06-28 16:38:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 provider "azurerm" {
-  ARM_CLIENT_ID ="${{ secrets.AZURE_CLIENT_ID }}"
+  ARM_CLIENT_ID = "${{ secrets.AZURE_CLIENT_ID }}"
   ARM_SUBSCRIPTION_ID = "${{ secrets.AZURE_SUBSCRIPTION_ID }}"
   ARM_TENANT_ID = "${{ secrets.AZURE_TENANT_ID }}"
 
