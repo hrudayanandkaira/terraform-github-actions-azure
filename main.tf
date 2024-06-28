7d96139fef72c59c0eeaa7b88eb427630066a1f4
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2024-06-28 15:12:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
       version = ">= 3.7.0"
     }
   }
-
+/*
   # Update this block with the location of your terraform state file
    backend "azurerm" {
     resource_group_name  = "azureautomation-rg"
@@ -13,12 +13,13 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
+*/
   }
 }
 
 provider "azurerm" {
   features {}
-  use_oidc = true
+  #use_oidc = true
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
