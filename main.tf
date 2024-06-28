2e53294e7fd3556bc68ea01a0feeb69b6443cce6
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2024-06-28 17:54:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ terraform {
 }
 
 provider "azurerm" {
-  #ARM_CLIENT_ID = "${{ secrets.AZURE_CLIENT_ID }}"
-  #ARM_SUBSCRIPTION_ID = "${{ secrets.AZURE_SUBSCRIPTION_ID }}"
-  #ARM_TENANT_ID = "9f2a5891-22b8-4308-92fa-fdef45ffb338" #"${{ secrets.AZURE_TENANT_ID }}"
-
+  client_id       = "c586ee23-af11-497c-8b73-22e8b785dda5"
+  client_secret   = variable.client_secret
+  tenant_id       = "9f2a5891-22b8-4308-92fa-fdef45ffb338"
+  subscription_id = "e520db69-a509-4dbf-9480-fa7a46560ac1"
   features {}
  # use_oidc = true
 }
