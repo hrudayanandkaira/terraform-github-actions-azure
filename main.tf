764880e064ebe5d2a8be834f7abd861d261e72db
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2024-06-28 19:35:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,10 @@ terraform {
 }
 
 provider "azurerm" {
+  client_id      = env.ARM_CLIENT_ID
+  client_secret = env.ARM_CLIENT_SECRET
+  tenant_id      = env.ARM_TENANT_ID
+  subscription_id = env.ARM_SUBSCRIPTION_ID
   features {}
   use_oidc = true
 }
